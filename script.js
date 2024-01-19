9bc5249215a32a4be82301c669249af80d87c7eb
Anastasia Dor <anastasia.dor@gmail.com>
2024-01-19 14:11:49 +0100

    Created home page, and transition from the home page to the earth sketch and history sketch

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('astronomy').addEventListener('click', function() {
+    window.location.href = 'earth.html';
+});
+
+document.getElementById('world-history').addEventListener('click', function() {
+    window.location.href = 'g7.html';
+});
