c18c624317528e691e8003864027d5c4c9885fe7
Anastasia Dor <anastasia.dor@gmail.com>
2024-01-19 21:18:02 +0100

    working state for show

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-document.getElementById('astronomy').addEventListener('click', function() {
-    window.location.href = 'earth.html';
-});
+if (window.location.pathname === '/index.html') {
+    document.getElementById('astronomy').addEventListener('click', function() {
+        window.location.href = 'earth.html';
+    });
 
 document.getElementById('world-history').addEventListener('click', function() {
     window.location.href = 'g7.html';
 });
 
+}
+
+
 // Assuming there's an element with the id 'win' in your HTML, otherwise remove this.
-document.getElementById('win').addEventListener('click', function() {
-    window.location.href = 'win.html';
-});
+// document.getElementById('win').addEventListener('click', function() {
+//     window.location.href = 'win.html';
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
     // Check if we're on the correct page for these elements.
-    if (document.getElementById('merkel') && document.getElementById('trash-icon')) {
+    if (document.getElementById('merkel') ) {
         const merkel = document.getElementById('merkel');
-        const trashIcon = document.getElementById('trash-icon');
+        // const trashIcon = document.getElementById('trash-icon');
         let isDragging = false;
 
         merkel.addEventListener('mousedown', function (e) {
@@ -26,14 +30,28 @@ document.addEventListener('DOMContentLoaded', function () {
             if (isDragging) {
                 merkel.style.left = e.pageX - merkel.width / 2 + 'px';
                 merkel.style.top = e.pageY - merkel.height / 2 + 'px';
-                checkOverlap();
+                // checkOverlap();
             }
         });
 
         document.addEventListener('mouseup', function (e) {
             isDragging = false;
+            const originalImage = document.getElementById('svg1');
+            originalImage.src = '2.svg'
+            merkel.style.display = 'none';
+            console.log('mouseup', originalImage.getAttribute('src'));
+
+            if (originalImage.getAttribute('src') === '2.svg') {
+                setTimeout(function() {
+                window.location.href = 'win.html';
+            }, 6000); // 60000 milliseconds = 1 minute
+            }
+
+     
+
         });
 
+        // this function is not being used
         function checkOverlap() {
             const merkelRect = merkel.getBoundingClientRect();
             const trashRect = trashIcon.getBoundingClientRect();
